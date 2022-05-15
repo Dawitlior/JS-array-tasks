07f80a6c41fd8f65ac394f32c1734effc115b296
Dawitlior <dawitlior777@gmail.com>
2022-05-16 00:38:52 +0300

    H

--- main.js
+++ main.js
@@ -16,28 +16,141 @@
 // console.log(numArray)
 
 
-// ליצור מערך חדש עם מספרים מ0-50 ולהדפיס לקונסול 
-// להדפיס לקונסול רק את המספרים הזוגיים
-// var numArray = [2,4,3,11,34,43,24,12,31,15]
-// console.log(numArray)
-// for(var i = 0; i<numArray.length; i++){
-//     if(numArray[i]%2==0){
-//         console.log(numArray[i])
-//     }
-// }
+// נתון המערך הבא 
+// var myArray = ["hello",2,"world","jacob",6,7,"bye",98];
+// הדפיסו רק את המילים
+// var temArray = []
+// for(var i = 0;i<myArray.length;i++){
+//     if(myArray[i] *1 != myArray[i]){
+//         console.log(myArray[i])
+
+
 
 
 
 
 // נתון המערך הבא 
-var myArray = ["hello",2,"world","jacob",6,7,"bye",98];
+// var myArray = ["hello",2,"world","jacob",6,7,"bye",98];
 // הדפיסו רק את המילים
-console.log(myArray);
-var temArray = []
-for(var i = 0;i<myArray.length;i++){
-    if(myArray[i] *1 != myArray[i]){
-        console.log(myArray[i])
+// console.log(myArray);
+// for(var i = 0;i<myArray.length;i++){
+    //     if(myArray[i] *1 != myArray[i]){
+        //         console.log(myArray[i])
+        
+        //     }
+        
+        // }
+        
+        
+        // פונקציית push
+        
+        // var someaArray = [];
+        // someaArray.push(9);
+        // someaArray.push(2);
+        // console.log(someaArray)
+        
+        
+        
+        // var someArray = [];
+        // for(var i = 0 ; i<10 ; i++){
+            //     someArray.push(i);
+            // }
 
-    }
+
+            // ליצור מערך חדש עם מספרים מ0-50 ולהדפיס לקונסול 
+// ליצור מערך חדש עם מספרים מ0-50 ולהדפיס לקונסול 
+            // ליצור מערך חדש עם מספרים מ0-50 ולהדפיס לקונסול 
+            // להדפיס לקונסול רק את המספרים הזוגיים
+            // var numArray = [2,4,3,11,34,43,24,12,31,15]
+            // console.log(numArray)
+            // for(var i = 0; i<numArray.length; i++){
+            //     if(numArray[i]%2==0){
+            //         console.log(numArray[i])
+            //     }
+            // }
+
+            // ------------------------------------------------------------
+            // ------------------------------------------------------------
+            // -------------------------HOMEWORKS-----------------------
+
+// function somNum(){
+//     var someNumbers = [5,3,7,8,11]
+//     console.log(someNumbers)
+//     var sum = 0
+//     for(var i = 0 ; i<someNumbers.length;i++){
+//         sum+=someNumbers[i]
+//         var avg = someNumbers[i]/5
+//     }
     
+//     console.log(sum,avg)
+    
+// }
+// somNum();
+
+
+// function somNam(){
+    // var someNames = ["lior","david","natan","eldad","yaso"]
+    // console.log(someNames);
+    // for(var i = 0 ; i<someNames.length; i++){
+    //     console.log(someNames[i])
+    // }
+// }
+// somName();
+
+
+
+
+
+
+
+// function myArray(num,array){
+//     var save = 0
+//     for(var i = 0 ; i < array.length ; i++ ){
+//         if(array[i]%2==0){
+//         save++
+//         }
+//     }
+//     console.log(save)
+// }
+// myArray(5,[2,6,4,7]);
+
+
+
+
+
+
+
+
+// function someArray(some1,some2){
+// for(var i = 0 ; i < some2.length ;i++){
+//     if(some1==some2[i]){
+//         return true
+//     }
+//     return false
+// }
+
+// }
+// console.log(someArray(4,[5,2,7,8]));
+
+
+
+
+// function myArray(num1,num2){
+// for(var i = 0 ; i<num2.length ; i++){
+//     if(num1==num2[i]){
+//         num2++
+//         return true
+//     }
+//     return false
+// }
+// }
+// console.log(myArray(8,[5,6,11,6,2]))
+
+
+function newArray(array,array1){
+if(array.length==array1.length){
+    return true
 }
+return false
+}
+newArray([1,3,2,4,7,6,5],[4,6,2,3,1,8,7])
