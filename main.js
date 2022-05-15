63b9f3606b5a182893c67671295e6d64137e5720
Dawitlior <dawitlior777@gmail.com>
2022-05-15 11:50:50 +0300

    new 15.5

---
+++ main.js
@@ -0,0 +1,9 @@
+var myArray = ["lior","david","yossi","eldad","taka"]
+console.log()
+console.log(myArray)
+console.log(myArray[3])
+
+
+var numArray = [500,400,300,200,600,423,711]
+console.log(numArray[4])
+console.log(numArray)
