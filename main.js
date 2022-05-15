a1b7fcdddad8248155df3b8d9b7387f7409d2adb
Dawitlior <dawitlior777@gmail.com>
2022-05-15 14:12:32 +0300

    ++

--- main.js
+++ main.js
@@ -1,9 +1,45 @@
-var myArray = ["lior","david","yossi","eldad","taka"]
-console.log()
-console.log(myArray)
-console.log(myArray[3])
+// var myArray = ["lior","david","yossi","eldad","taka"]
+// console.log()
+// console.log(myArray)
+// console.log(myArray[3])
+// for(var i=0; i>myArray.length;i++){
+
+//     console.log(myArray[i]);
+// }
+
+// console.log(myArray)
 
 
-var numArray = [500,400,300,200,600,423,711]
-console.log(numArray[4])
-console.log(numArray)
+
+// var numArray = [500,400,300,200,600,423,711]
+// console.log(numArray[4])
+// console.log(numArray)
+
+
+// ליצור מערך חדש עם מספרים מ0-50 ולהדפיס לקונסול 
+// להדפיס לקונסול רק את המספרים הזוגיים
+// var numArray = [2,4,3,11,34,43,24,12,31,15]
+// console.log(numArray)
+// for(var i = 0; i<numArray.length; i++){
+//     if(numArray[i]%2==0){
+//         console.log(numArray[i])
+//     }
+// }
+
+
+
+
+// נתון המערך הבא 
+// הדפיסו רק את המילים
+
+
+var myArray = ["hello",2,"world","jacob",6,7,"bye",98];
+console.log(myArray);
+var temArray = []
+for(var i = 0;i<myArray.length;i++){
+    if(myArray[i] *1 != myArray[i]){
+        console.log(myArray[i])
+
+    }
+    
+}
